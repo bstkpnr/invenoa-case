07327a24fbc29aadaeff5d5cc7acee6ec74c67cb
Beste Tokpınar <55047664+bstkpnr@users.noreply.github.com>
2023-07-06 13:10:07 +0300

    create data

---
+++ src/data/data.ts
@@ -0,0 +1,127 @@
+
+const data=[
+    {
+        name: 'Bültenim',
+        img:require('../assets/star.png'),
+        subMenus: [
+            {
+                id:1,
+                subMenuName:'Futbol',
+                img:require('../assets/soccer-ball-variant.png')
+            },
+            {
+                id:2,
+                subMenuName:'Voleybol',
+                img:require('../assets/volleyball.png')
+            },
+            {
+                id:3,
+                subMenuName:'Hentbol',
+                img:require('../assets/handball.png')
+            },
+            {
+                id:4,
+                subMenuName:'Buz Hokeyi',
+                img:require('../assets/ice-hockey.png')
+            },
+            {
+                id:5,
+                subMenuName:'Uzun Vadeli',
+                img:require('../assets/long-term.png')
+            }
+        ],
+      },
+      {
+        name: 'CANLI',
+        img:require('../assets/foam-hand.png'),
+        subMenus: [
+            {
+                id:1,
+                subMenuName:'Futbol',
+                img:require('../assets/soccer-ball-variant.png')
+            },
+            {
+                id:2,
+                subMenuName:'Voleybol',
+                img:require('../assets/volleyball.png')
+            },
+            {
+                id:3,
+                subMenuName:'Hentbol',
+                img:require('../assets/handball.png')
+            },
+            {
+                id:4,
+                subMenuName:'Buz Hokeyi',
+                img:require('../assets/ice-hockey.png')
+            },
+            {
+                id:5,
+                subMenuName:'Uzun Vadeli',
+                img:require('../assets/long-term.png')
+            }
+        ],      },
+      {
+        name: 'MAÇÖNÜ',
+        img:require('../assets/motivation.png'),
+        subMenus: [
+            {
+                id:1,
+                subMenuName:'Futbol',
+                img:require('../assets/soccer-ball-variant.png')
+            },
+            {
+                id:2,
+                subMenuName:'Voleybol',
+                img:require('../assets/volleyball.png')
+            },
+            {
+                id:3,
+                subMenuName:'Hentbol',
+                img:require('../assets/handball.png')
+            },
+            {
+                id:4,
+                subMenuName:'Buz Hokeyi',
+                img:require('../assets/ice-hockey.png')
+            },
+            {
+                id:5,
+                subMenuName:'Uzun Vadeli',
+                img:require('../assets/long-term.png')
+            }
+        ],
+      },
+      {
+        name: 'Filtre',
+        img:require('../assets/filter.png'),
+        subMenus: [
+            {
+                id:1,
+                subMenuName:'Futbol',
+                img:require('../assets/soccer-ball-variant.png')
+            },
+            {
+                id:2,
+                subMenuName:'Voleybol',
+                img:require('../assets/volleyball.png')
+            },
+            {
+                id:3,
+                subMenuName:'Hentbol',
+                img:require('../assets/handball.png')
+            },
+            {
+                id:4,
+                subMenuName:'Buz Hokeyi',
+                img:require('../assets/ice-hockey.png')
+            },
+            {
+                id:5,
+                subMenuName:'Uzun Vadeli',
+                img:require('../assets/long-term.png')
+            }
+        ],      },
+]
+
+export {data}
